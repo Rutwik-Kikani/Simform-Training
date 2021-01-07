716a948d23cb5e849afb9f2607fce163890bc864
Rutwik <kikanirutwik001@gmail.com>
2021-01-07 14:37:37 +0530

    commit_07-01-2021

--- JSPractice/js-calculator/script.js
+++ JSPractice/js-calculator/script.js
@@ -8,43 +8,122 @@ let display = document.getElementById("display");
 let isDisplayHasResult = false;
 
 //reset display function
-function resetDisplay(){
-    display.value = 0;
-}
-//clear display function
-function clearDisplay(){
-    display.value='';
+function resetDisplay() {
+  display.value = 0;
 }
 
-//todo: on esc key press clear the display.
+//dummy function;
+function foo(Element) {
+  console.log("this is call the foo()" + Element);
+}
 
-//this is a function which will display any data from local storage
-display.addEventListener("onload",function(){
-    //todo: check data is on local storage or not. and print it on display
-});
+function removeFirstZeros(str) {
+  // console.log(str.length);
+  return str.replace(/^0+/, "");
+}
+
+function setToLocalStorage(item) {
+  localStorage.setItem("valDisplay", item);
+}
+function getFromLocalStorage() {
+  return localStorage.getItem("valDisplay");
+}
+
+function doWhenButtonClicked(calc_btn) {
+  //when button other then c and = is clicked, display value should change
+  switch (calc_btn.id) {
+    case "equal":
+      {
+        // console.log(calc_btn);
+        //print result on display and set flag to true
+        try{
+        //before going to evaluation remove first 0 why? try 010 without removing zero and press = ;
+            let finalEvalString = removeFirstZeros(display.value);
+            display.value = eval(finalEvalString);
+            isDisplayHasResult = true;
+        } catch(error){
+            console.log(error);
+            display.value = "Error";
+        }
+      }
+      break;
+    case "clear":
+      {
+        // console.log(calc_btn);
+        //do reset display
+        resetDisplay();
+      }
+      break;
+    default: {
+      display.value += calc_btn.textContent;
+    }
+  }
+  setToLocalStorage(display.value);
+}
+
+function init() {
+  resetDisplay();
+  const valueOfDisplay = getFromLocalStorage();
+  if (valueOfDisplay) {
+    display.value = valueOfDisplay;
+  }
+}
+init();
+
+// 2. this approch did not work why? doubt
+//this is a function which will call on display load
+// display.addEventListener("load",function(){
+//     console.log("Loaded");
+// });
+
+//3. this approch did not work why? becouse i set input field as readonly conform this with mentor
+//add onchange eventlistner to display so that you can add it to localstorage
+// display.addEventListener("oninput", function(){
+//     console.log("display value is "+ display.value);
+// });
 
 //add click event listener to all button
-for(let i=0;i<calc_buttons.length;i++){
-    calc_buttons[i].addEventListener("click", function(){
-        //when button other then c and = is click, display value should change
-        switch(calc_buttons[i].id){
-            case "equal": {
-                //do print result on display and set flag to true
-                //todo: try and catch the exceptions
-                display.value = eval(display.value);
-                isDisplayHasResult = true;
-                // console.log(calc_buttons[i]);
-            }break;
-            case "clear":{
-                //do reset display
-                resetDisplay()
-                // console.log(calc_buttons[i]);
-            }break;
-            default:{
-                    display.value += calc_buttons[i].textContent;    
-            }
-
-        }
-       
-    });
+for (let i = 0; i < calc_buttons.length; i++) {
+  calc_buttons[i].addEventListener("click", function () {
+    //when button is press
+    doWhenButtonClicked(calc_buttons[i]);
+  });
 }
+
+//Esc key press display should be reset
+const validKeyArray = ["1","2","3","4","5","6","7","8","9","0",".","Enter","Escape","+","-","/","*",];
+
+document.onkeydown = function (event) {
+  //this line will help you to get when 1,2,3 etc key press how event looks like
+  // console.log(event);
+
+  evt = event || window.event;
+  console.log("key" + evt.key + "pressed");
+  if (validKeyArray.indexOf(evt.key)>=0) {
+    switch (evt.key) {
+      case "Enter":
+        {
+          let equal_btn = document.getElementById("equal");
+          doWhenButtonClicked(equal_btn);
+        }
+        break;
+      case "Escape":
+      case "Esc":
+        {
+          resetDisplay();
+        }
+        break;
+      default: {
+        //do nothing;
+        let btn = document.getElementById(evt.key);
+        btn.classList.add("active");
+        setTimeout(() => {
+            btn.classList.remove("active");
+        },100);
+        doWhenButtonClicked(btn);        
+      }
+    }
+  }else{
+      alert("Invalid Key Press!!");
+  }
+};
