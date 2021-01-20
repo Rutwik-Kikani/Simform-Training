c5ec9a481f62022d063216ada02c43fac6222731
Rutwik <kikanirutwik001@gmail.com>
2021-01-20 20:08:46 +0530

    structure_for_demo_app

--- ReactPractice/simple-react-setup/src/containers/App.js
+++ ReactPractice/simple-react-setup/src/containers/App.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { Component } from "react";
 import styles from "./App.module.css";
-import thumb from "../assets/img/thumb1.jpg";
 import './App.css';
 
-//Import Components
-import Nav from "../components/Navigation/Nav";
-
-const App = () => (
-  <div className="App">
-    <h1 className={styles.wow}>Hello React!!!!!</h1>
-  </div>
-);
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <header>
+        <h2>header</h2>
+      </header>
+      
+        <h1 className={styles.natsu}>To Do App</h1>
+      <footer>
+        <h2>footer</h2>
+      </footer>
+      </div>
+      
+    );
+  }
+}
 
 export default App;
