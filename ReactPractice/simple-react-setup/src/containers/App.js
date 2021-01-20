cb66611ce2786ebd9d335128d245b784e5bbe5f7
Rutwik <kikanirutwik001@gmail.com>
2021-01-20 16:58:41 +0530

    set_up_react_app_from_scratch

---
+++ ReactPractice/simple-react-setup/src/containers/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./App.module.css";
+import thumb from "../assets/img/thumb1.jpg";
+
+//Import Components
+import Nav from "../components/Navigation/Nav";
+
+const App = () => (
+  <div>
+    <h1 className={styles.wow}>Hello React!!!!!</h1>
+  </div>
+);
+
+export default App;
