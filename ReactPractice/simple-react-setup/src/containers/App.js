a6eb60ad916e6a9ff9e0720285b0dd6460407f9c
Rutwik <kikanirutwik001@gmail.com>
2021-01-20 19:17:45 +0530

    setup_todo_with_webpack

--- ReactPractice/simple-react-setup/src/containers/App.js
+++ ReactPractice/simple-react-setup/src/containers/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import styles from "./App.module.css";
 import thumb from "../assets/img/thumb1.jpg";
+import './App.css';
 
 //Import Components
 import Nav from "../components/Navigation/Nav";
 
 const App = () => (
-  <div>
+  <div className="App">
     <h1 className={styles.wow}>Hello React!!!!!</h1>
   </div>
 );
--- ReactPractice/simple-react-setup/src/index.js
+++ ReactPractice/simple-react-setup/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import './index.css';
 //Import App
 import App from "./containers/App";
 
