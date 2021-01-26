1fe96e5a58f176a47da0881118b7956bc3b4ff59
Rutwik <kikanirutwik001@gmail.com>
2021-01-26 17:52:16 +0530

    setup_to_add_dynamic_ingredient_component

--- ReactPractice/burger-app/src/App.js
+++ ReactPractice/burger-app/src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 // import styles from './App.module.css';
+
+//? import components
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 class App extends Component {
   render() {
     return (
-      <div>
-        Hello World!!
-      </div>
+      <Layout>
+        <BurgerBuilder/>
+      </Layout>
     );
   }
 }
