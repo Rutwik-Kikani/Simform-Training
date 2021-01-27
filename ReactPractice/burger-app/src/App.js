478967401c338c201f1081b6cb4183017577db33
Rutwik <kikanirutwik001@gmail.com>
2021-01-27 11:39:29 +0530

    setup_and_basic_structure

--- ReactPractice/burger-app/src/App.js
+++ ReactPractice/burger-app/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import styles from './App.module.css'
+
+import Layout from './components/Layout/Layout';
+import BurgerBuillder from './containers/BurgerBuilder/BurgerBuillder';
+ class App extends Component {
+  render(){
+    return (
+    
+     <Layout>
+       <BurgerBuillder/>
+     </Layout>
+    
+  );}
 }
 
 export default App;
---
+++ ReactPractice/burger-app/src/containers/BurgerBuilder/BurgerBuillder.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+import Aux from '../../hoc/Auxiliary';
+
+import Burger from '../../components/Burger/Burger';
+export default class BurgerBuillder extends Component {
+    render() {
+        return (
+            <Aux>   
+                <Burger></Burger>
+                
+                <div>Burger Controls</div>
+            </Aux>
+        )
+    }
+}
