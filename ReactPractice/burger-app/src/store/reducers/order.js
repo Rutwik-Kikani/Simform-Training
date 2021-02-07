185afad961bcb2448688b01fd68b44fcd5da7787
Rutwik <kikanirutwik001@gmail.com>
2021-02-07 18:57:04 +0530

    add_with_redux_advance

--- ReactPractice/burger-app/src/store/reducers/order.js
+++ ReactPractice/burger-app/src/store/reducers/order.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionsTypes';
+import { updateObject } from '../utility';
 
 const initalState = {
     orders: [],
@@ -9,34 +10,84 @@ const initalState = {
 const reducer = (state = initalState ,action) => {
     switch(action.type){
         case actionTypes.PURCHASE_INIT:
-            return{
-                ...state,
-                purchased: false,
-            }
+            return updateObject(state, {purchased: false});
+            //1.
         case actionTypes.PURCHASE_BURGER_START:
-            return{
-                ...state,
-                loading: true,
-            }
+            return updateObject(state,{loading:true});
+            //2.
         case actionTypes.PURCHASE_BURGER_SUCCESS:
-            const newOrder = {
-                ...action.orderData,
-                id: action.orderId,
-            }
-            return{
-                ...state,
+            const newOrder = updateObject(action.orderData, {id: action.orderId});
+            return updateObject(state, {
                 loading: false,
                 orders: state.orders.concat(newOrder),
                 purchased: true,
-            }
+            })
+            //3.
         case actionTypes.PURCHASE_BURGER_FAIL:
-            return{
-                ...state,
+            return updateObject(state, { loading: false, })
+            //4.
+        case actionTypes.FETCH_ORDERS_START:
+            return updateObject(state, { loading: true })
+            //5.
+        case actionTypes.FETCH_ORDERS_SUCCESS:
+            return updateObject(state, {
+                orders: action.orders,
                 loading: false,
-            }
+            })
+            // 6.
+        case actionTypes.FETCH_ORDERS_FAIL:
+            return updateObject(state, { loading: false, })
+            //7.
         default:
             return state;
     }
 }
 
 export default reducer;
+
+/**************************************************************************** 
+ * 1.
+ * return{
+                ...state,
+                purchased: false,
+            }
+
+    2.
+    return{
+                ...state,
+                loading: true,
+            }
+    3.
+    // const newOrder = {
+            //     ...action.orderData,
+            //     id: action.orderId,
+            // }
+            // return{
+            //     ...state,
+            //     loading: false,
+            //     orders: state.orders.concat(newOrder),
+            //     purchased: true,
+            // }
+    4.
+    return{
+                ...state,
+                loading: false,
+            }
+    5.
+    return{
+                ...state,
+                loading: true,
+            }
+    6.
+    return{
+            //     ...state,
+            //     orders: action.orders,
+            //     loading: false,
+            // }
+    7.
+    return{
+                ...state,
+                loading: false,
+            }
+ * 
+*/
